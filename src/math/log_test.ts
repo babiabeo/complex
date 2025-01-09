ee2105ddb480b87f7436a80f650997755737fa45
babiabeo <ducthangn71007@gmail.com>
2025-01-09 16:23:08 +0700

    ready for v2

--- src/math/log_test.ts
+++ src/math/log_test.ts
@@ -3,7 +3,6 @@ import { assertAlmostEquals } from "@std/assert";
 import { complex } from "../complex.ts";
 import { log, log10 } from "./log.ts";
 
-
 test("Complex natural logarithm", () => {
     const ex = log(new complex(3, 4));
 
