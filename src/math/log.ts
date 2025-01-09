2ebec9cbcdb9fd174a0e75540a21addad719fc64
babiabeo <ducthangn71007@gmail.com>
2025-01-09 20:26:30 +0700

    make log() more accurate

--- src/math/log.ts
+++ src/math/log.ts
@@ -10,7 +10,26 @@ import { complex } from "../complex.ts";
  * ```
  */
 export function log(z: complex): complex {
-    const [r, phi] = z.polar();
+    const x = Math.abs(z.real);
+    const y = Math.abs(z.imag);
+    const r = Math.hypot(x, y);
+    const phi = Math.atan2(z.real, z.imag);
+
+    // if sqrt(0.5) <= r <= sqrt(3), use log1p() for more accurate results
+    //
+    //                                 log(x^2 + y^2)   log1p((x^2 - 1) + y^2)
+    // log(r) = log(sqrt(x^2 + y^2)) = -------------- = ----------------------
+    //                                        2                   2
+    if (0.71 <= r && r <= 1.73) {
+        const a = Math.max(x, y);
+        const b = Math.min(x, y);
+
+        return new complex(
+            Math.log1p((a - 1) * (a + 1) + b * b) / 2,
+            phi,
+        );
+    }
+
     return new complex(Math.log(r), phi);
 }
 
